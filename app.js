667db42f8c66e4ac8a303c2885793302fa6abd96
Your Name <you@example.com>
2020-05-15 14:13:38 -0300

    server estaticos y agregando nodemon

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+const server = express()
+
+const docs = express.static("docs")
+
+
+
+server.use( docs )
+
+server.get("/index.html", function(request,response){
+    response.end("hola desde Node.js + Express")
+})
+
+server.get("/mercadotech.html", function(request,response){
+    response.end("aca debo ver el catalogo")
+})
+
+
+//server.post("")
+
+server.listen(80)
