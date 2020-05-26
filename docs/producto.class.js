d416b43f42e0d694814c3c271b410da8a1a4a627
Your Name <you@example.com>
2020-05-25 22:23:07 -0300

    implementando metodos de clase

--- docs/producto.class.js
+++ docs/producto.class.js
@@ -1,4 +1,4 @@
-class producto {
+class Producto {
 
     constructor(n,s,p,i,m){
         this.nombre = n
@@ -6,38 +6,45 @@ class producto {
         this.precio = p
         this.imagen = i
         this.marca = m
-
     }
-     
-     Mostrar(){
-        const ficha = document.createElement("article")
-        
-        
-            ficha.classList.add("col-4")
 
-            ficha.innerHTML=`<div class="card h-100">
-                    <a href="#">
-                        <img class="card-img-top img-fluid" src="${this.imagen}" alt="">
-                    </a>
-                    <div class="card-body">
-                        <h4 class="card-title"><a href="#">${this.nombre}-${this.marca} </a> <span class="badge badge-pill badge-success float-right">$${this.precio}</span></h4>
-                        <p class="card-text">${this.stock} unid.</p>
-                        <button class="btn btn-primary float-right">Comprar</button>
-                    </div>
-                </div> `
 
-        document.querySelector("#productos-destacados").appendChild(ficha)
-     }
 
-     descuento (cupon){ //<---metodo de instancia
-        if(cupon  == UHTXTU78I){
-            this .precio -=(this.precio * 0.15) 
-        }
-     } 
-        ////////////////////////////////////////
-         static armarCatalago(){//<---metodos de clases(o estaticos)
+Mostrar(){
+   
+    const ficha = document.createElement("article")
+    
+    
+        ficha.classList.add("col-4")
 
-        }
+        ficha.innerHTML =`<div class="card h-100">
+                <a href="#">
+                    <img class="card-img-top img-fluid" src="${this.imagen}" alt="">
+                </a>
+                <div class="card-body">
+                    <h4 class="card-title"><a href="#">${this.nombre}-${this.marca} </a> <span class="badge badge-pill badge-success float-right">$${this.precio}</span></h4>
+                    <p class="card-text">${this.stock} unid.</p>
+                    <button class="btn btn-primary float-right">Comprar</button>
+                </div>
+            </div> `
+
+    document.querySelector("#productos-destacados").appendChild(ficha)
+ 
+  }
+   
+
+
+/////////////////////////////////////////////
+
+ static armarCatalogo(objetos, rango){//<---metodos de clases(o estaticos)
+   
+    let productos = objetos.map(({Nombre,Stock,Precio,Imagen,Marca}) => new Producto(Nombre,Stock,Precio,Imagen,Marca))
+
+	let resultado = productos.filter(producto => producto.precio > 249 && producto.stock > 100)
+
+					return resultado
+
+
+
+ }
 }
-
-    ///////////////////
